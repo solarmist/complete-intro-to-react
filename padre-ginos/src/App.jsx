f015f57392736cd83bc9f480cd70298c6872576f
Joshua Olson <joshua.olson+github@gmail.com>
2024-12-30 12:44:18 -0800

    Step 06 - custom-hooks

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,5 +1,7 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 
 const App = () => {
@@ -7,10 +9,14 @@ const App = () => {
     <div>
       <h1>Padre Gino's Pizza</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<StrictMode>
+  <App />
+</StrictMode>
+);
---
+++ padre-ginos/src/usePizzaOfTheDay.jsx
@@ -0,0 +1,19 @@
+import { useDebugValue, useState, useEffect } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
