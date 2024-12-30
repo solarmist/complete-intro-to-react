ca40998edc8b642a9fe49a32dbaa12b143bd5240
Joshua Olson <joshua.olson+github@gmail.com>
2024-12-30 15:08:48 -0800

    Step 10 - query

--- padre-ginos/eslint.config.mjs
+++ padre-ginos/eslint.config.mjs
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
@@ -16,6 +17,7 @@ export default [
     },
   },
   reactPlugin.configs.flat["jsx-runtime"],
+  ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
